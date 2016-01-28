9a891d84c8e17b527aa0ab3b23d8a04e27b59ce2
LOZORD <ljrudberg@gmail.com>
2016-01-28 13:25:34 -0600

    More work on the Exis nav directive

--- static/templates/directives/exisNav.js
+++ static/templates/directives/exisNav.js
@@ -1,6 +1,20 @@
 angular.module('exisNavModule', [])
 .directive('exisNav', [function() {
   return {
+    restrict: 'E',                  // must be an element
+    scope: {
+      bgColor: '@backgroundColor',  // for basic coloring
+      background: '@'               // for gradients (e.g. Swift)
+    },
+    controller: function($scope) {  // set some default values
+      if (!$scope.bgColor && !$scope.background) {
+        $scope.bgColor = '#282B35';
+      }
+
+      if (!$scope.background) {
+        $scope.background = $scope.bgColor;
+      }
+    },
     templateUrl: '../static/templates/exis-nav.html'
   };
 }]);
